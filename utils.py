6cb1b54c2e330947bb33d27e5543387203aa2dee
snsuma05-dotcom <sn.suma05@gmail.com>
2026-07-25 15:14:30 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,13 @@
+import fitz
 
+def read_pdf(pdf):
+
+    text = ""
+
+    document = fitz.open(stream=pdf.read(), filetype="pdf")
+
+    for page in document:
+
+        text += page.get_text()
+
+    return text
